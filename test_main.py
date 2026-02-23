efeeba7a72a6cfbf953c32e98fecbd3629b6ddbe
67319010003-bit <67319010003@phuketvc.ac.th>
2026-02-23 16:55:05 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,6 +2,6 @@ import unittest
 		from main import add
 		class TestAdd(unittest.TestCase):
   		def test_add(self):
-        		self.assertEqual(add(2, 3), 5)
+        		self.assertEqual(add(2, 3), 6)
 		if __name__ == '__main__':
    		 unittest.main()
